1d3c45081e45daf30ab92935c5bfd7ac3ddeec9b
royanfauzan <royanfauzan7@gmail.com>
2021-01-15 19:39:35 +0800

    ubah dikit

--- src/tokoCampingClient/FormPengembalian.java
+++ src/tokoCampingClient/FormPengembalian.java
@@ -11,6 +11,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,6 +21,8 @@ import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
@@ -30,6 +33,8 @@ import javax.swing.JTextArea;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.Cursor;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class FormPengembalian extends JFrame {
 
@@ -37,22 +42,35 @@ public class FormPengembalian extends JFrame {
 	private JTable table;
 	private String header[] = {"Nama Pelanggan","Nama Barang","Jumlah","Tanggal Selesai","No Telp"};
 	private DefaultTableModel tabelModel;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
 	private JLabel lblTanggal;
-	private DateFormat dateFormat;
-	public Date tanggalSkrg;
-	private JTextField textFieldKembali;
+	private JLabel lblNewLabelRusak;
+	private JLabel lblNewLabelHilang;
+	
+	private JComboBox comboBoxKondisi;
+
+	private JTextField textFieldIdPel;
+	private JTextField textFieldIdBar;
+	private JTextField textFieldJmlPinjam;
 	private JTextField textFieldRusak;
 	private JTextField textFieldHilang;
 	
 	private ArrayList<String> listMulai;
 	private ArrayList<String> idBarang;
+	private ArrayList<Integer> idPelanggan;
 	private ArrayList<Integer> idPesanan;
 	private ArrayList<Integer> listHarga;
 	private ArrayList<Integer> listStok;
 	private ArrayList<Integer> gantiRugi;
+	
+	private DateFormat dateFormat;
+	public Date tanggalSkrg,tanggalLese;
+	
+	private String TbIdBar;
+	private int currIdPelanggan,idBefore,currIdPesan,currStok;
+	private int jmlPesan,lamaPinjam,hargaPerBar,totalBayar;
+	private int lambat,denda,hilang,rusak;
+	
+	private String kalimat;
 
 	/**
 	 * Launch the application.
@@ -85,6 +103,7 @@ public class FormPengembalian extends JFrame {
 	public FormPengembalian() {
 		
 
+		idPelanggan = new ArrayList<Integer>();
 		idPesanan = new ArrayList<Integer>();
 		idBarang = new ArrayList<String>();
 		listMulai = new ArrayList<String>();
@@ -92,6 +111,8 @@ public class FormPengembalian extends JFrame {
 		listStok = new ArrayList<Integer>();
 		gantiRugi = new ArrayList<Integer>();
 		
+		kalimat = "";
+		idBefore = 0;
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 626, 668);
@@ -109,10 +130,44 @@ public class FormPengembalian extends JFrame {
 		
 		tabelModel = new DefaultTableModel(null,header);
 		table = new JTable();
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				int i = table.getSelectedRow();
+				
+				comboBoxKondisi.setSelectedIndex(0);
+				hilang = 0;
+				rusak = 0;
+				
+				lambat = 0;
+				currIdPesan = idPesanan.get(i);
+				currIdPelanggan = idPelanggan.get(i);
+				currStok = listStok.get(i);
+				
+				hargaPerBar = listHarga.get(i);
+				TbIdBar = idBarang.get(i);
+				jmlPesan = (int)table.getValueAt(i, 2);
+				String tglSelesai = (String)table.getValueAt(i, 3);
+				
+				try {
+					tanggalLese = dateFormat.parse(tglSelesai);
+					
+					long miliLambat = tanggalSkrg.getTime()-tanggalLese.getTime();
+					lambat = (int)Math.floor(TimeUnit.DAYS.convert(miliLambat, TimeUnit.MILLISECONDS));
+				} catch (Exception e) {
+					// TODO: handle exception
+					e.printStackTrace();
+				}
+				
+				textFieldIdBar.setText(TbIdBar);
+				textFieldJmlPinjam.setText(""+jmlPesan);
+			}
+		});
 		table.setModel(tabelModel);
 		scrollPane.setViewportView(table);
 		
 		try {
+			kosongkanTabel();
 			getDataPeminjam();
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -134,10 +189,11 @@ public class FormPengembalian extends JFrame {
 		lblNewLabel.setBounds(216, 11, 196, 35);
 		contentPane.add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(21, 71, 115, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		textFieldIdPel = new JTextField();
+		textFieldIdPel.setBounds(21, 71, 115, 20);
+		contentPane.add(textFieldIdPel);
+		textFieldIdPel.setColumns(10);
+		
 		
 		JButton btnNewButton_1 = new JButton("Cari");
 		btnNewButton_1.setBounds(146, 70, 89, 23);
@@ -162,15 +218,16 @@ public class FormPengembalian extends JFrame {
 		lblNewLabel_3_1.setBounds(21, 410, 89, 14);
 		contentPane.add(lblNewLabel_3_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(135, 381, 71, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		textFieldIdBar = new JTextField();
+		textFieldIdBar.setBounds(135, 381, 71, 20);
+		contentPane.add(textFieldIdBar);
+		textFieldIdBar.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(135, 407, 47, 20);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		
+		textFieldJmlPinjam = new JTextField();
+		textFieldJmlPinjam.setBounds(135, 407, 47, 20);
+		contentPane.add(textFieldJmlPinjam);
+		textFieldJmlPinjam.setColumns(10);
 		
 		lblTanggal = new JLabel("Tanggal : "+dateFormat.format(tanggalSkrg));
 		lblTanggal.setFont(new Font("Tahoma", Font.BOLD, 12));
@@ -190,9 +247,33 @@ public class FormPengembalian extends JFrame {
 		lblNewLabel_5.setBounds(165, 348, 46, 14);
 		contentPane.add(lblNewLabel_5);
 		
-		JButton btnNewButton_2 = new JButton("Konfirmasi");
-		btnNewButton_2.setBounds(135, 539, 100, 23);
-		contentPane.add(btnNewButton_2);
+		JButton btnKonfirmasi = new JButton("Konfirmasi");
+		btnKonfirmasi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if (!textFieldIdBar.getText().equals("")) {
+					
+					if (currIdPelanggan!=idBefore) {
+						kalimat = "iD Pelanggan :"+currIdPelanggan+"\n";
+						kalimat+="Pesanan id "+currIdPesan+" Terkonfirmasi \n";
+						
+						idBefore = currIdPelanggan;
+					} else {
+						kalimat+="Pesanan id "+currIdPesan+" Terkonfirmasi \n";
+					}
+					
+					
+				} else {
+					JOptionPane.showMessageDialog(null, "Pilih Data Dari Tabel!") ;
+				}
+				
+
+				
+				
+			}
+		});
+		btnKonfirmasi.setBounds(135, 514, 100, 23);
+		contentPane.add(btnKonfirmasi);
 		
 		JScrollPane scrollPane_1 = new JScrollPane();
 		scrollPane_1.setBounds(287, 368, 282, 250);
@@ -201,39 +282,69 @@ public class FormPengembalian extends JFrame {
 		JTextArea textArea = new JTextArea();
 		scrollPane_1.setViewportView(textArea);
 		
-		textFieldKembali = new JTextField();
-		textFieldKembali.setBounds(135, 438, 47, 20);
-		contentPane.add(textFieldKembali);
-		textFieldKembali.setColumns(10);
-		
 		textFieldRusak = new JTextField();
-		textFieldRusak.setBounds(135, 469, 47, 20);
+		textFieldRusak.setBounds(135, 483, 47, 20);
 		contentPane.add(textFieldRusak);
 		textFieldRusak.setColumns(10);
 		
 		textFieldHilang = new JTextField();
-		textFieldHilang.setBounds(135, 500, 47, 20);
+		textFieldHilang.setBounds(203, 483, 47, 20);
 		contentPane.add(textFieldHilang);
 		textFieldHilang.setColumns(10);
 		
-		JLabel lblNewLabel_4 = new JLabel("Jumlah Kembali");
+		JLabel lblNewLabel_4 = new JLabel("Kondisi Barang");
 		lblNewLabel_4.setBounds(21, 441, 100, 14);
 		contentPane.add(lblNewLabel_4);
 		
-		JLabel lblNewLabel_6 = new JLabel("Jumlah Rusak");
-		lblNewLabel_6.setBounds(21, 472, 89, 14);
-		contentPane.add(lblNewLabel_6);
+		lblNewLabelRusak = new JLabel("Rusak");
+		lblNewLabelRusak.setBounds(135, 469, 37, 14);
+		contentPane.add(lblNewLabelRusak);
 		
-		JLabel lblNewLabel_7 = new JLabel("Jumlah Hilang");
-		lblNewLabel_7.setBounds(21, 503, 89, 14);
-		contentPane.add(lblNewLabel_7);
+		lblNewLabelHilang = new JLabel("Hilang");
+		lblNewLabelHilang.setBounds(203, 469, 47, 14);
+		contentPane.add(lblNewLabelHilang);
+		
+		comboBoxKondisi = new JComboBox();
+		comboBoxKondisi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (comboBoxKondisi.getSelectedIndex()==1) {
+					lblNewLabelRusak.setVisible(true);
+					lblNewLabelHilang.setVisible(true);
+					
+					textFieldRusak.setVisible(true);
+					textFieldHilang.setVisible(true);
+				} else {
+					lblNewLabelRusak.setVisible(false);
+					lblNewLabelHilang.setVisible(false);
+					
+					textFieldRusak.setVisible(false);
+					textFieldRusak.revalidate();
+					textFieldRusak.repaint();
+					
+					textFieldHilang.setVisible(false);
+					textFieldHilang.revalidate();
+					textFieldHilang.repaint();
+				}
+			}
+		});
+		comboBoxKondisi.setModel(new DefaultComboBoxModel(new String[] {"Lengkap", "Tidak Lengkap"}));
+		comboBoxKondisi.setBounds(135, 438, 115, 20);
+		contentPane.add(comboBoxKondisi);
+		
+		
+		textFieldIdBar.disable();
+		textFieldJmlPinjam.disable();
+		lblNewLabelRusak.setVisible(false);
+		lblNewLabelHilang.setVisible(false);
+		textFieldHilang.setVisible(false);
+		textFieldRusak.setVisible(false);
 	}
 	
 	private void getDataPeminjam() {
 		try {
 			Connection konek = Koneksi.getKoneksi();
 			Statement state = konek.createStatement();
-			String query = "SELECT pelanggan.nama_pelanggan,barang.nama_barang,pesanan.jumlah_pesanan,pesanan.tanggal_selesai,pelanggan.no_telp,pelanggan.id_pelanggan,barang.id_barang,pesanan.tanggal_mulai,barang.harga_barang,barang.stok_barang,barang.ganti_rugi FROM pelanggan INNER JOIN pesanan ON pelanggan.id_pelanggan=pesanan.id_pelanggan INNER JOIN barang ON pesanan.id_barang = barang.id_barang WHERE pesanan.`tanggal_kembali` IS NULL ORDER BY pesanan.`tanggal_selesai`";
+			String query = "SELECT pelanggan.nama_pelanggan,barang.nama_barang,pesanan.jumlah_pesanan,pesanan.tanggal_selesai,pelanggan.no_telp,pelanggan.id_pelanggan,barang.id_barang,pesanan.tanggal_mulai,barang.harga_barang,barang.stok_barang,barang.ganti_rugi,pesanan.id_pesanan FROM pelanggan INNER JOIN pesanan ON pelanggan.id_pelanggan=pesanan.id_pelanggan INNER JOIN barang ON pesanan.id_barang = barang.id_barang WHERE pesanan.`tanggal_kembali` IS NULL ORDER BY pesanan.`tanggal_selesai`";
 			ResultSet rs = state.executeQuery(query);
 			while(rs.next())
 			{
@@ -245,12 +356,14 @@ public class FormPengembalian extends JFrame {
 				obj[3] = rs.getString(4);
 				obj[4] = rs.getString(5);
 				
-				idPesanan.add(rs.getInt(6));
+				idPelanggan.add(rs.getInt(6));
 				idBarang.add(rs.getString(7));
 				listMulai.add(rs.getString(8));
 				listHarga.add(rs.getInt(9));
 				listStok.add(rs.getInt(10));
 				gantiRugi.add(rs.getInt(11));
+				idPesanan.add(rs.getInt(12));
+				
 				
 				tabelModel.addRow(obj);
 			}
@@ -267,7 +380,7 @@ public class FormPengembalian extends JFrame {
 		tabelModel.fireTableDataChanged();
 		
 		listMulai.removeAll(listMulai);
-		idPesanan.removeAll(idPesanan);
+		idPelanggan.removeAll(idPelanggan);
 		idBarang.removeAll(idBarang);
 		listHarga.removeAll(listHarga);
 		listStok.removeAll(listStok);
